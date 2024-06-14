a6452c22750a53f42bb7b88bd195ac881e7f69ff
Agostina <agosqdo@gmail.com>
2024-06-14 15:48:09 -0300

    Inicio reparado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,36 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = () => (
-    <header>
-        <div className="logo-container">
-            <Link to="/">
-                <img src="/images/logo_solo.png" alt="K-ON Dance Studio Logo" className="logo" />
-                <img src="/images/titulo.png" alt="K-ON Dance Studio Title" className="logo-title" />
-            </Link>
-        </div>
-        <nav>
-            <ul className="nav-links">
-                <li><Link to="/classes">Clases</Link></li>
-                <li><Link to="/groups">Grupos</Link></li>
-                <li><Link to="/proyectos">Proyectos</Link></li>
-                <li><Link to="/contact">Contacto</Link></li>
-            </ul>
-        </nav>
-    </header>
-);
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <header>
+            <div className="logo-container">
+                <Link to="/">
+                    <img src="/images/logo_solo.png" alt="K-ON Dance Studio Logo" className="logo" />
+                    <img src="/images/titulo.png" alt="K-ON Dance Studio Title" className="logo-title" />
+                </Link>
+            </div>
+            <nav>
+                <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                    <li><Link to="/">Inicio</Link></li>
+                    <li><Link to="/classes">Clases</Link></li>
+                    <li><Link to="/groups">Grupos</Link></li>
+                    <li><Link to="/proyectos">Proyectos</Link></li>
+                    <li><Link to="/contact">Contacto</Link></li>
+                </ul>
+                <div className="burger" onClick={handleToggle}>
+                    ☰
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
