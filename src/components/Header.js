c373eb4a78c594cd29f235d7f4bf7aa22523d686
Agostina <agosqdo@gmail.com>
2024-06-14 15:26:07 -0300

    Estilos cambiados

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,33 +5,29 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 import './Header.css';
 
 const Header = () => {
-    const [isMobile, setIsMobile] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-    const handleMenuClick = () => {
-        setIsMobile(!isMobile);
-    };
-
-    const closeMobileMenu = () => {
-        setIsMobile(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
     };
 
     return (
         <header>
             <div className="logo-container">
-                <Link to="/" onClick={closeMobileMenu}>
+                <Link to="/">
                     <img src="/images/logo_solo.png" alt="K-ON Dance Studio Logo" className="logo" />
                     <img src="/images/titulo.png" alt="K-ON Dance Studio Title" className="logo-title" />
                 </Link>
             </div>
             <nav>
-                <div className="mobile-menu-icon" onClick={handleMenuClick}>
-                    {isMobile ? <FaTimes /> : <FaBars />}
+                <div className="menu-icon" onClick={toggleMenu}>
+                    {isOpen ? <FaTimes /> : <FaBars />}
                 </div>
-                <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
-                    <li><Link to="/classes" onClick={closeMobileMenu}>Clases</Link></li>
-                    <li><Link to="/groups" onClick={closeMobileMenu}>Grupos</Link></li>
-                    <li><Link to="/projects" onClick={closeMobileMenu}>Proyectos</Link></li> 
-                    <li><Link to="/contact" onClick={closeMobileMenu}>Contacto</Link></li>
+                <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                    <li><Link to="/classes" onClick={toggleMenu}>Clases</Link></li>
+                    <li><Link to="/groups" onClick={toggleMenu}>Grupos</Link></li>
+                    <li><Link to="/proyectos" onClick={toggleMenu}>Proyectos</Link></li>
+                    <li><Link to="/contact" onClick={toggleMenu}>Contacto</Link></li>
                 </ul>
             </nav>
         </header>
