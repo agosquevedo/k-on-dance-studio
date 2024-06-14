53d2d9a5498d9cdbcd1a15db84b4a96e543c528d
Agostina <agosqdo@gmail.com>
2024-06-14 15:15:58 -0300

    Menu desplegable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,41 @@
 // src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import './Header.css';
 
-const Header = () => (
-    <header>
-        <div className="logo-container">
-            <Link to="/">
-                <img src="/images/logo_solo.png" alt="K-ON Dance Studio Logo" className="logo" />
-                <img src="/images/titulo.png" alt="K-ON Dance Studio Title" className="logo-title" />
-            </Link>
-        </div>
-        <nav>
-            <ul>
-                <li><Link to="/classes">Clases</Link></li>
-                <li><Link to="/groups">Grupos</Link></li>
-                <li><Link to="/proyectos">Proyectos</Link></li> 
-                <li><Link to="/contacto">Contacto</Link></li>
-            </ul>
-        </nav>
-    </header>
-);
+const Header = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsMobile(!isMobile);
+    };
+
+    const closeMobileMenu = () => {
+        setIsMobile(false);
+    };
+
+    return (
+        <header>
+            <div className="logo-container">
+                <Link to="/" onClick={closeMobileMenu}>
+                    <img src="/images/logo_solo.png" alt="K-ON Dance Studio Logo" className="logo" />
+                    <img src="/images/titulo.png" alt="K-ON Dance Studio Title" className="logo-title" />
+                </Link>
+            </div>
+            <nav>
+                <div className="mobile-menu-icon" onClick={handleMenuClick}>
+                    {isMobile ? <FaTimes /> : <FaBars />}
+                </div>
+                <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+                    <li><Link to="/classes" onClick={closeMobileMenu}>Clases</Link></li>
+                    <li><Link to="/groups" onClick={closeMobileMenu}>Grupos</Link></li>
+                    <li><Link to="/projects" onClick={closeMobileMenu}>Proyectos</Link></li> 
+                    <li><Link to="/contact" onClick={closeMobileMenu}>Contacto</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
